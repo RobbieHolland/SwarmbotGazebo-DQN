14cfd3a3b78e878adbdc22ab6be84d5ad3241ae5
Robert Holland <rh2515@ic.ac.uk>
2016-08-19 15:41:14 +0100

    Bare bones Environment and Model for A3C

--- SwarmbotModel.lua
+++ SwarmbotModel.lua
@@ -5,28 +5,28 @@ local Body = classic.class('Body')
 
 -- Constructor
 function Body:_init(opts)
-	--[[
+	
   opts = opts or {}
 
   self.recurrent = opts.recurrent
   self.histLen = opts.histLen
   self.stateSpec = opts.stateSpec
-	--]]
 end
 
 function Body:createBody()
-	--[[
-  -- Number of input frames for recurrent networks is always 1
   local histLen = self.recurrent and 1 or self.histLen
+
   local net = nn.Sequential()
-  net:add(nn.View(histLen*self.stateSpec[2][1], self.stateSpec[2][2], self.stateSpec[2][3]))
-  net:add(nn.SpatialConvolution(histLen*self.stateSpec[2][1], 32, 5, 5, 2, 2, 1, 1))
-  net:add(nn.ReLU(true))
-  net:add(nn.SpatialConvolution(32, 32, 5, 5, 2, 2))
-  net:add(nn.ReLU(true))
+  net:add(nn.View(4, 1, 15, 2)) -- Concatenate history in channel dimension
+  --net:add(nn.SpatialConvolution(4, 32, 8, 8, 4, 4, 1, 1))
+  --net:add(nn.SpatialConvolution(32, 4, 4, 4, 2, 2))
+
+	--For testing purposes
+	net:add(nn.View(120))
+	net:add(nn.Linear(120, 4))
 
   return net
-	--]]
+	
 end
 
 return Body
