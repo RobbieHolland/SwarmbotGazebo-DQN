a890536e2b99064a8514eaa9b4f2a8caad63f11e
Robert Holland <rh2515@ic.ac.uk>
2016-08-22 13:49:43 +0100

    Updated SwarmbotModel's nn to use View->SpatialConvolution->ReLU

--- SwarmbotModel.lua
+++ SwarmbotModel.lua
@@ -10,6 +10,7 @@ function Body:_init(opts)
   self.recurrent = opts.recurrent
   self.histLen = opts.histLen
   self.stateSpec = opts.stateSpec
+	self.hiddenSize = opts.hiddenSize
 
 end
 
@@ -17,11 +18,8 @@ function Body:createBody()
   local histLen = self.recurrent and 1 or self.histLen
 
   local net = nn.Sequential()
-	--Concatenate history in channel dimension
-	--2D input tensor for temporal convolution
-  net:add(nn.View(self.histLen, 1*15*2))
-	--Each frame has size 1*15*2, outputFrameSize is 4 (is this the number of outputs of the network?), kernel size is 4, kernel step is 1
-  net:add(nn.TemporalConvolution(1*15*2, 4, 4, 1))
+  net:add(nn.View(self.histLen, 1, 15*2))
+  net:add(nn.SpatialConvolution(self.histLen, 16, 3, 1))
   net:add(nn.ReLU(true))
 
   return net
