d1ce83677bcdf113cc79ac268932986c8acd2eaa
Kaixhin <design@kaixhin.com>
2016-08-30 14:20:57 +0100

    Beginning of a command buffer to sync all robots and Atari code

---
+++ command_buffer.lua
@@ -0,0 +1,9 @@
+--ROS stuff
+
+--Subscribe to command topics advertised by environments
+
+--Keep spinning until all messages have been received
+
+--Send off all commands to robots
+
+--Send message to topic signifying that commands have been sent
